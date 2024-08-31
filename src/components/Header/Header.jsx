056d5858d2cedd2d697b8d8825088fe35b7cdcf8
Ezaz <majumderezaz@gmail.com>
2024-09-01 00:47:38 +0530

    starting

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <h1>EZAZ MAJUMDER</h1>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import "./home.css";
+import Header from "../../components/Header/Header";
+
+const Home = () => {
+  let [images, setNewImages] = useState([]);
+  useEffect(() => {
+    fetch("/backgroundimg.json")
+      .then((response) => response.json())
+      .then((jsonData) => setNewImages(jsonData))
+      .catch((error) =>
+        console.log("Error while fetching the json datar", console.error)
+      );
+  }, []);
+
+  return (
+    <>
+      <div className="home-container">
+        <div className="background">
+          {images.map((image, i) => (
+            <img src={image.image} alt="" />
+          ))}
+        </div>
+      </div>
+          <Header />
+    </>
+  );
+};
+
+export default Home;
