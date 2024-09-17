8a1775ba5f2a48768b4bad0c19ad90f53fdb5ce8
Ezaz <majumderezaz@gmail.com>
2024-09-17 18:41:30 +0530

    d

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,13 +5,26 @@ const Header = () => {
   return (
     <div className="header-container" id="Header">
       <div className="back-img"></div>
-      <div  className="heading-body">
+      <div className="heading-body">
         <div data-aos="fade-right" className="heading-container">
-          <h3>'Hellow', I Am,</h3>
-          <h1>EZAZ</h1>
-          <h2>MAJUMDER</h2>
+          <h3>Hi, I'm,</h3>
+          <h1>EZAZ MAJUMDER</h1>
+          <h2>-Front-End Developer</h2>
+          <p>
+            I specialize in building interactive, user-friendly, and responsive
+            web applications. With a strong foundation in HTML, CSS, and
+            JavaScript, along with expertise in modern frameworks like React.js,
+            I turn creative ideas into functional digital experiences.
+          </p>
+          <a href="/cv.pdf" download="Ezaz-cv.pdf">
+            <button className="download-btn-cv">Download CV</button>
+          </a>
         </div>
-        <div data-aos="fade-left" data-aos-duration="1000"  className="img-block">
+        <div
+          data-aos="fade-left"
+          data-aos-duration="1000"
+          className="img-block"
+        >
           <div className="selfimg">
             <img src="/images/ezaz1.png" alt="my img" />
           </div>
